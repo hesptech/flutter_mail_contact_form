66b34089a3e6326d5d947c9365b8404018f9ac70
oswaldo olea <o.olea@sintraconsulting.com>
2023-05-16 10:16:22 +0200

    feat: emailjs setup working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:http/http.dart' as http;
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,60 +22,153 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final messageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
+      backgroundColor: const Color(0xfff5f5fd),
       body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          height: 450,
+          width: 400,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 40,
+            vertical: 20,
+          ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+            vertical: 20,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    offset: const Offset(0, 5),
+                    blurRadius: 10,
+                    spreadRadius: 1,
+                    color: Colors.grey[300]!)
+              ]),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text('Contact Us',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(hintText: 'Name'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '*Required';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(hintText: 'Email'),
+                  validator: (email) {
+                    if (email == null || email.isEmpty) {
+                      return 'Required*';
+                    } else if (!EmailValidator.validate(email)) {
+                      return 'Please enter a valid Email';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: messageController,
+                  decoration: const InputDecoration(hintText: 'Message'),
+                  maxLines: 5,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '*Required';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 45,
+                  width: 110,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: const Color(0xff151534),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40))),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final response = await sendEmail(
+                            nameController.value.text,
+                            emailController.value.text,
+                            messageController.value.text);
+                        /* ScaffoldMessenger.of(context).showSnackBar(
+                          response == 200
+                              ? const SnackBar(
+                                  content: Text('Message Sent!'),
+                                  backgroundColor: Colors.green)
+                              : const SnackBar(
+                                  content: Text('Failed to send message!'),
+                                  backgroundColor: Colors.red),
+                        ); */
+                        print(response);
+                        if ( response == 200 ) {
+                          //const Text('Message Sent!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.red),);
+                          print('success');
+                          print(response);
+                        } else {
+                          //const Text('Failed to send message!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.red),);
+                          print('failure');
+                          print(response);
+                        }
+                        nameController.clear();
+                        emailController.clear();
+                        messageController.clear();
+                      }
+                    },
+                    child: const Text('Send', style: TextStyle(fontSize: 16)),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
+}
+
+Future sendEmail(String name, String email, String message) async {
+  final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+  final response = await http.post(url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-Type': 'application/json'
+      },
+      body: json.encode({
+        'service_id': 'service_m6t6vgr',
+        'template_id': 'template_wz3p33d',
+        'user_id': 'xg_GS8-YofnjsS8Jb',
+        'template_params': {
+          'from_name': name,
+          'from_email': email,
+          'message': message
+        }
+      }));
+  return response.body;
 }
